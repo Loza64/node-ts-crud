c89223e8415dcc115a2b2a6710e61b8395c7e203
= <=>
2026-09-23 17:57:10 -0600

    update cors config

--- src/shared/config/express.config.ts
+++ src/shared/config/express.config.ts
@@ -1,9 +1,11 @@
 import { env } from './env';
 
+const origin = env.ORIGIN ? env.ORIGIN.trim().split(',') : '*'
+
 export const corsConfig = {
-  origin: env.ORIGIN || '*',
+  origin: origin,
   credentials: !!env.ORIGIN,
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Internal-Api-Key'],
 };
 
 export const jsonConfig = {
